c008eb48c1d082cca8cb2b8106495f6d0c9d596f
Irizkyw <ichwanrizkywahyudin3@gmail.com>
2022-07-16 13:42:34 +0700

    konfigurasi client dan api-server

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  // res.send('respond with a resource');
+  res.json([
+    {
+      id: 1,
+      username: "A",
+    },
+    {
+      id: 2,
+      username: "B",
+    },
+    {
+      id : 3,
+      username : "C"
+    },
+    {
+      id : 4,
+      username : "D"
+    }
+  ]);
 });
 
 module.exports = router;
