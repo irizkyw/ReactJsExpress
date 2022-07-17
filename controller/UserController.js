0d034978742cecb0dbcae2461d1de164a1dfc1af
Irizkyw <ichwanrizkywahyudin3@gmail.com>
2022-07-17 20:43:35 +0700

    validator di routes dan POST user

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,9 @@
 const models = require("./../models");
+const Validator = require("fastest-validator");
+
 const User = models.users;
+
+const v = new Validator();
 
 class UserController {
   static async getAll(req, res) {
@@ -11,42 +15,68 @@ class UserController {
     }
   }
 
-  static async getID(req,res){
-    try{
-        const id = req.params.id;
-        const users = await User.findByPk(id);
-        return res.json(users || {});
-    }catch{
-        return res.status(500).json({error : err.message})
+  static async postUsers(req, res) {
+    const schema = {
+      name: "string ",
+      email: "string ",
+      password: "string ",
+      roles_id: "int",
+    };
+    try {
+      const validate = v.validate(req.body,schema);
+      if (validate.length){
+        return res.status(500).json(validate)
+      }
+      const users = await User.create(req.body);
+      return res.json(users)
+    } catch {
+      return res.status(500).json({ error: err.message });
     }
   }
 
-  static async putEdit(req,res){
-    try{
+  static async getID(req, res) {
+    try {
       const id = req.params.id;
+      const users = await User.findByPk(id);
+      return res.json(users || {});
+    } catch {
+      return res.status(500).json({ error: err.message });
+    }
+  }
+
+  static async putEdit(req, res) {
+    const schema = {
+      name: "string | optional",
+      email: "string | optional",
+      password: "string | optional",
+      roles_id: "optional",
+    };
+    try {
+      const id = req.params.id;
+      const validate = v.validate(req.body, schema);
       let users = await User.findByPk(id);
-      if (!users){
-        return res.json({message : 'user not found'})
+      if (!users) {
+        return res.json({ message: "user not found" });
       }
       users = await users.update(req.body);
-      return res.json(users)
-    }catch{
-      return res.status(500).json({error : err.message})
+      return res.json(users);
+    } catch {
+      return res.status(500).json({ error: err.message });
     }
   }
 
-  static async getDelete(req,res){
-    try{
+  static async getDelete(req, res) {
+    try {
       const id = req.params.id;
 
       let users = await User.findByPk(id);
-      if (!users){
-        return res.json({message : 'user not found'})
+      if (!users) {
+        return res.json({ message: "user not found" });
       }
       await users.destroy();
-      res.json(500).json({message : 'success'})
-    }catch{
-      return res.status(500).json({error : err.message})
+      res.json(500).json({ message: "success" });
+    } catch {
+      return res.status(500).json({ error: err.message });
     }
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,8 @@ const router  = express.Router();
 router.get('/api', UserController.getAll);
 router.get('/api/search/:id',UserController.getID);
 router.get('/api/delete/:id',UserController.getDelete);
-router.get('/api/update/:id',UserController.putEdit);
+router.put('/api/update/:id',UserController.putEdit);
+router.post('/api/insert',UserController.postUsers);
 
 
 module.exports = router;
